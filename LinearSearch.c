793a0864ae0bec848982c18addffd69c8c00bc96
Sai Immani <saichowdaryimmani@gmail.com>
2025-01-24 22:23:38 +0530

    Update LinearSearch.c

--- LinearSearch.c
+++ LinearSearch.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+// Function to perform Linear Search
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i; // Target found at index i
+        }
+    }
+    return -1; // Target not found
+}
+
+int main() {
+    int arr[] = {2, 3, 4, 10, 40};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 10;
+
+    int result = linearSearch(arr, size, target);
+
+    if (result != -1) {
+        printf("Element is present at index %d\n", result);
+    } else {
+        printf("Element is not present in array\n");
+    }
+
+    return 0;
+}
