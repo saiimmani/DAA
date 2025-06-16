72a08dbc1cf963f0bc64568a569f8f35be766a6b
Sai Immani <saichowdaryimmani@gmail.com>
2025-06-16 21:44:59 +0530

    Create RandomizedQuickSort.cpp

---
+++ RandomizedQuickSort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high], i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+int randomPartition(int arr[], int low, int high) {
+    int random = low + rand() % (high - low + 1);
+    swap(arr[random], arr[high]);
+    return partition(arr, low, high);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = randomPartition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    srand(time(0));
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    return 0;
+}
